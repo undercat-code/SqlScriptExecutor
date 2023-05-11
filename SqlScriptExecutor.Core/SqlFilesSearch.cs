2621aebc91f5956c88d8e6a62d9dc22cd63e9b86
Undercat-CODE <seven.russia@gmail.com>
2023-05-11 10:53:37 +0300

    Add project files.

---
+++ SqlScriptExecutor.Core/SqlFilesSearch.cs
@@ -0,0 +1,27 @@
+﻿namespace SqlScriptExecutor.Core
+{
+    class SqlFilesSearch
+    {
+        public SqlFilesSearch(string path) => Path = path;
+
+        public string Path { get; set; }
+
+
+        //Return all sql files from main directory and subdirectories
+        public IEnumerable<string> GetAllSqlFiles()
+
+        {
+            IEnumerable<string> searchingFilesResult = null;
+
+            try
+            {
+                searchingFilesResult = Directory.EnumerateFiles(Path, "*.sql", SearchOption.AllDirectories);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return searchingFilesResult;
+        }
+    }
+}
